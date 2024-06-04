8af146e416b9f2ee8ffc1c440c38def7fa5daf72
Your Name <you@example.com>
2024-06-04 14:48:35 +0200

    first push

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controllerBlague = require("./../controller/Blague");
+const controllerBlague = require("../controller/blague");
 
 router.get("/", (req, res) => {
   res.status(200).json({ success: "RACINE API" });
