90d1e489d342813592098a1a4c3755b1b74204ac
Your Name <you@example.com>
2024-06-04 15:14:40 +0200

    first push

--- serveur.js
+++ serveur.js
@@ -1,30 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-const connexion = require("./DB/dbConnect");
-const app = express();
-const port = 3000;
-const version = "v1";
-const router = require("./routes/routes");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-const options = require("./swagger.json");
+const express = require('express');
+const app = express()
+const port = 3000
+const version = "v1"
+const router = require('./routes/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = require('./swagger.json');
 const specs = swaggerJsdoc(options);
-const Blague = require("./model/Blague");
-const { SELECT } = require("sequelize/lib/query-types");
 
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(`/examapi/${version}`, router);
-app.use(
-  `/examapi/${version}/api-docs`,
-  swaggerUi.serve,
-  swaggerUi.setup(specs, { explorer: true })
-);
+app.use(express.urlencoded({extended: true}));
+app.use(`/api/${version}`, router);
+app.use( `/api/${version}/api-docs`, swaggerUi.serve, swaggerUi.setup(specs, { explorer : true }));
 
-connexion.sync().then(() => {
-  console.log("DBconnect est synchronisé");
-  app.listen(port, () => {
-    console.log("Example app listening on port ${port}");
-  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 });
